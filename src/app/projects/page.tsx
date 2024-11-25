bddebe86fb36c302eb0c6932592bca6f0d6268e6
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-11-25 15:25:12 +0700

    feat : add project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,8 +2,8 @@ import type { FC } from "react";
 import Project from "@/features/Projects";
 
 export const metadata = {
-  title: "Projects | Ulinnaja Aldi",
-  description: "Projects page of Aldilla Ulinnaja portfolio website",
+  title: "Projects | Alfthrpy",
+  description: "Projects page of Al-Fathir portfolio website",
 };
 
 const ProjectPage: FC = () => {
--- src/utils/datas.ts
+++ src/utils/datas.ts
@@ -3,60 +3,36 @@ import htmlIcon from "@icons/html.svg";
 import cssIcon from "@icons/css.svg";
 import jsIcon from "@icons/js.svg";
 import phpIcon from "@icons/php.svg";
-import sassIcon from "@icons/sass.svg";
 import bootstrapIcon from "@icons/bootstrap.svg";
 import tailwindIcon from "@icons/tailwind.svg";
 import reactIcon from "@icons/react.svg";
 import nextjsIcon from "@icons/nextjs.svg";
-import threejsIcon from "@icons/threejs.svg";
 import laravelIcon from "@icons/laravel.svg";
-import nodejsIcon from "@icons/nodejs.svg";
 import npmIcon from "@icons/npm.svg";
-import yarnIcon from "@icons/yarn.svg";
-import blenderIcon from "@icons/blender.svg";
-import figmaIcon from "@icons/figma.svg";
 import gitIcon from "@icons/git.svg";
 import githubIcon from "@icons/github.svg";
 import mysqlIcon from "@icons/mysql.svg";
-import splineIcon from "@icons/spline.svg";
 import vscodeIcon from "@icons/vscode.svg";
-import vite from "@icons/vite.svg";
-import webpackIcon from "@icons/webpack.svg";
 import expressIcon from "@icons/express.svg";
 import mongodbIcon from "@icons/mongodb.svg";
 import typescriptIcon from "@icons/typescript.svg";
-import reactQueryIcon from "@icons/react-query.svg";
-import formikIcon from "@icons/formik.png";
 import pythonIcon from "@icons/python.svg";
 import PandasIcon from "@icons/pandas.jpg";
 import JavaIcon from "@icons/java.svg";
 
 // Project Images
-import mulih from "@images/mulih.webp";
-import travel from "@images/travel.webp";
-import bookshelf from "@images/bookshelf.webp";
-import notes from "@images/notes.webp";
-import treede from "@images/3d.webp";
-import abinawa from "@images/abinawa.webp";
-import inpoums from "@images/inpoums.webp";
-import portfolio_v1 from "@images/portfolio-v1.webp";
-import bertumbuh from "@images/bertumbuh.webp";
-import coffe_bean from "@images/coffe-bean.webp";
-import dapbunda from "@images/dapbunda.webp";
-import hpcjapan from "@images/hpc-japan.webp";
-import ceritain from "@images/ceritain.webp";
-import pinarak_resto from "@images/pinarak_resto.webp";
+import iris from "@images/iris.png";
+import Ebuddy from "@images/ebuddy.png";
+import sendthepict from "@images/sendthepict.png";
+import tts from "@images/tts.png";
+import mikku from "@images/Mikku.png";
+import searchmantic from "@images/searchmantic.png";
 
 // Gif Images
-import dapbundaGif from "@gif/dapbunda.gif";
-import coffebeanGif from "@gif/coffe-bean.gif";
-import portfolioGif from "@gif/portfolio.gif";
-import notesGif from "@gif/notes.gif";
-import abinawaGif from "@gif/abinawa.gif";
-import bookshelfGif from "@gif/bookshelf.gif";
-import sakurajapanGif from "@gif/sakura-japan.gif";
-import ceritainGif from "@gif/ceritain.gif";
-import pinarakrestoGif from "@gif/pinarak-resto.gif";
+import mikkuGif from "@gif/mikku.gif";
+import ttsGif from "@gif/tts.gif";
+import ebuddyGif from "@gif/ebuddy.gif";
+import searchmanticGif from "@gif/searchmantic.gif";
 
 const tools = {
   Git: { name: "Git", src: gitIcon, level: "Version control" },
@@ -86,144 +62,64 @@ const stacks = {
 
 const projects = [
   {
-    id: 1,
-    name: "Mulih!",
-    desc: "My first project was created using HTML, CSS, and JavaScript. This website was created to fulfill the final assignment for the Basics of Web Programming course from Dicoding ⭐5.",
-    image: mulih,
-    web: "https://mulih.netlify.app/",
-    repo: "https://github.com/ulinnajaaldi/Dicoding-Mulih",
-    stack: "HTML, CSS, JavaScript, Responsive Web Design",
-    gif: "",
-  },
-  {
-    id: 2,
-    name: "Travel.",
-    desc: "This website serves as a platform to showcase hidden tourism spots in Bali, Indonesia. In addition to featuring these hidden gems, the website also offers an online booking system.",
-    image: travel,
-    web: "https://travel-apps.ulinnajaaldi.repl.co",
-    repo: "https://github.com/ulinnajaaldi/Vacation-Ticket-PHP",
-    stack: "HTML, Javascript, PHP, MySQL, TailwindCSS, DaisyUI, AOS, SwiperJS",
-    gif: "",
-  },
-  {
-    id: 3,
-    name: "Bookshelf",
-    desc: "This is a website that allows users to keep track of books they have read or plan to read, utilizing DOM modification and CRUD operations with data storage in LocalStorage ⭐5.",
-    image: bookshelf,
-    web: "https://bookshelf-js.vercel.app",
-    repo: "https://github.com/ulinnajaaldi/Bookshelf-JS",
-    stack: "HTML, Javascript, Bootstrap, SASS, ToastifyJS",
-    gif: bookshelfGif,
-  },
-  {
-    id: 4,
-    name: "3D World Journey",
-    desc: "Learning process using spline with react for Interactive 3D Object Web projects",
-    image: treede,
-    web: "https://ulinnaja-3-d-apps-intective.vercel.app",
-    repo: "https://github.com/ulinnajaaldi/React-3D-Object-Intective",
-    stack: "React, React-spline, React-router, SwiperJS, TailwindCSS, DaisyUI",
-    gif: "",
-  },
-  {
-    id: 5,
-    name: "Abinawa",
-    desc: "This website is designed as a digital learning tool to introduce the art of wayang kulit to the public. The website features interactive 3D models of wayang kulit.",
-    image: abinawa,
-    web: "http://abinawa.vercel.app",
-    repo: "https://github.com/ulinnajaaldi/abinawa",
-    stack: "Vite, React, React-spline, React-router, TailwindCSS",
-    gif: abinawaGif,
-  },
-  {
-    id: 6,
-    name: "Inpo UMS",
-    desc: "The purpose of this platform is to provide students with a medium to share information about the latest events or news happening in the UMS community.",
-    image: inpoums,
-    web: "",
-    repo: "https://github.com/ulinnajaaldi/laravel-react-inpo-ums",
-    stack: "Laravel, Laravel Breeze, React, React-icons, TailwindCSS, DaisyUI",
-    gif: "",
-  },
-  {
-    id: 7,
-    name: "Notes",
-    desc: "Welcome to Notes app! With this app, you can create, edit, delete and organize your notes. You can also sign up and log in to access your notes. This app I made for complete the Dicoding React Fundamental course ⭐5.",
-    image: notes,
-    web: "https://notes-app-api-db-dicoding.vercel.app",
-    repo: "https://github.com/ulinnajaaldi/Notes",
-    stack: "React, React-Icons, React-Router, Rest API, TailwindCSS, DaisyUI",
-    gif: notesGif,
-  },
-  {
-    id: 8,
-    name: "Portfolio",
-    desc: "My first personal portfolio!! I designed it by combining different ideas from various sources. It also has a secret game that you can play on your desktop. Can you find it? 🛸",
-    image: portfolio_v1,
-    web: "https://ulinnaja-aldi.vercel.app/",
-    repo: "https://github.com/ulinnajaaldi/portofolio",
-    stack: "NextJS, TailwindCSS, ThreeJS, React-spline",
-    gif: portfolioGif,
-  },
-  {
     id: 9,
-    name: "Bertumbuh (Slice)",
-    desc: "Trying to slice from Figma correctly to create a responsive website, and actually, it is already very good for being responsive (can be checked through inspect), but sometimes on certain screens, the margin is not read properly.",
-    image: bertumbuh,
+    name: "Iris Classifier",
+    desc: "The Iris Classifier app predicts the species of iris flowers (Setosa, Versicolor, Virginica) based on petal and sepal measurements, offering quick and accurate results.",
+    image: iris,
     web: "https://bertumbuh.vercel.app/",
     repo: "",
-    stack: "React, React-Icons, React-Router, TailwindCSS",
+    stack: "Python, Tkinter, Scikit-learn",
     gif: "",
   },
   {
     id: 10,
-    name: "Coffe Bean",
-    desc: "Interactive website featuring a 3D model that can be rendered online. I wanted to apply the creative idea I had, even if it was just from a picture ✨, I hope you enjoy the enjoyable experience on the website!",
-    image: coffe_bean,
-    web: "https://coffee-bean-3d.vercel.app/",
-    repo: "https://github.com/ulinnajaaldi/coffee-bean",
-    stack: "Vite, React, React-Icons, TailwindCSS, Spline, Framer Motion",
-    gif: coffebeanGif,
+    name: "EBuddy",
+    desc: "Ebuddy is an app developed for a Software Engineering project, designed to help government agencies create letters, attendance records, and out-of-office reports efficiently.",
+    image: Ebuddy,
+    web: "https://github.com/Alfthrpy/ebuddy-rpl",
+    repo: "https://github.com/Alfthrpy/ebuddy-rpl",
+    stack: "Laravel, Bootstrap",
+    gif: ebuddyGif,
   },
   {
     id: 11,
-    name: "DapBunda Meal",
-    desc: "This website provides information about various meals and how to cook them 🍴, using the API from TheMealDB. I created this website to complete the Dicoding Front End Fundamental course.",
-    image: dapbunda,
-    web: "https://dapbunda.vercel.app/",
-    repo: "https://github.com/ulinnajaaldi/dicoding-dapbunda-fe-fundamental",
-    stack: "Webpack, Axios, TailwindCSS, DaisyUI, AOS",
-    gif: dapbundaGif,
+    name: "SendThePict",
+    desc: "The website lets users send anonymous messages or letters by submitting images with a description of the message and recipient, offering a creative and private way to communicate.",
+    image: sendthepict,
+    web: "https://sendthepict.vercel.app/",
+    repo: "https://github.com/alfthrpy/sendthepict",
+    stack: "Webpack, Axios, TailwindCSS, DaisyUI",
+    gif: mikkuGif,
   },
   {
     id: 12,
-    name: "Sakura Japan HPC",
-    desc: "This website was crafted during my role as a speaker at Himatif Portable Club (HPC) 2023, aimed at introducing basic web development. If you're keen on learning how to build this site, please visit the project repository 🚀",
-    image: hpcjapan,
-    web: "https://sakura-japan-hpc.vercel.app/",
-    repo: "https://github.com/ulinnajaaldi/sakura-japan-hpc",
-    stack: "HTML, Javascript, TailwindCSS, Custom Element Javascript",
-    gif: sakurajapanGif,
+    name: "TTS Apps",
+    desc: "This website was crafted for lecture assignments, TTS Web is an interactive platform for solving crossword puzzles. It offers a fun and educational experience for puzzle enthusiasts of all ages. ",
+    image: tts,
+    web: "https://tts-app-eta.vercel.app/",
+    repo: "https://github.com/alfthrpy/tts-app",
+    stack: "Next.js, React.js, TailwindCSS, DaisyUI",
+    gif: ttsGif,
   },
   {
     id: 13,
-    name: "Ceritain!",
-    desc: "This website was developed as part of the final assignment for the Intermediate Front-End Web Tools course by Dicoding ⭐4. It is designed to enable users to share their stories, photos, and current location.",
-    image: ceritain,
-    web: "https://ceritain-app.web.app/",
-    repo: "https://github.com/ulinnajaaldi/story-app-fe-intermediate",
-    stack: "Javascript, Webpack, Axios, LitElement, Saas, Bootstrap, Firebase",
-    gif: ceritainGif,
+    name: "Mikku",
+    desc: "This website was developed to support the social-emotional development of children aged 3-4. Through inspiring stories, interactive games, and age-appropriate content.",
+    image: mikku,
+    web: "https://mikku-inky.vercel.app/",
+    repo: "https://github.com/alfthrpy/mikku",
+    stack: "Next.js, React.js, DaisyUI, Tailwind",
+    gif: mikkuGif,
   },
   {
     id: 14,
-    name: "Pinarak Resto",
-    desc: "This website was developed as part of the final assignment for the Front-End Web Developer Expert course by Dicoding ⭐5, and this website serves as an informational platform where users can comment on each restaurant.",
-    image: pinarak_resto,
-    web: "https://pinarak-resto.netlify.app/",
-    repo: "https://github.com/ulinnajaaldi/dicoding-pinarak-fe-expert",
-    stack: "Javascript, Webpack, PWA, IDB, CodeceptJs, Jasmine, Karma, Sass",
-    gif: pinarakrestoGif,
+    name: "Searchmantic Hadits",
+    desc: "Semantic-based Search Engine Platform For Hadits. Searchmantic Hadits is a search engine designed to help users quickly and accurately find hadith using keyword-based searches.",
+    image: searchmantic,
+    web: "https://searchmantic-hadist.vercel.app/",
+    repo: "https://github.com/alfthrpy/searchmantic-hadist",
+    stack: "Next.js, React.js, Flask, TensorFlow, Pandas, Python",
+    gif: searchmanticGif,
   },
 ];
 
